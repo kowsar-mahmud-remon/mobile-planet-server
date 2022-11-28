d22af58db41281ceb92150b4da8c52f0cb796c01
creativerze <creativerze@gmail.com>
2022-11-28 20:47:56 +0600

    added product delete api

--- index.js
+++ index.js
@@ -115,6 +115,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get('/orders', async (req, res) => {
       const email = req.query.email;
       console.log(email);
